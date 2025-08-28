e0e00c912b5ee062167842b7b4e57f42293e48ed
naymulislamnay <naymulislamnay@gmail.com>
2025-08-28 23:00:20 +0600

    history and clear button functionality added

--- scripts/script.js
+++ scripts/script.js
@@ -11,11 +11,41 @@ for (let loveIcon of loveIcons) {
 // function for call button
 
 const callBtns = document.getElementsByClassName('call-btn');
+const historyContainer = document.getElementById('history-container');
 
 for (let callBtn of callBtns) {
     callBtn.addEventListener('click', function () {
+        const serviceMainName = callBtn.parentNode.parentNode.childNodes[3].innerText;
         const serviceName = callBtn.parentNode.parentNode.childNodes[5].innerText;
         const serviceNumber = callBtn.parentNode.parentNode.childNodes[7].innerText;
+        const now = new Date();
+        const localTime = now.toLocaleTimeString();
         alert(`📞 Calling ${serviceName} ${serviceNumber}...`)
+
+        // function for history
+        const newHistory = document.createElement('div');
+        newHistory.innerHTML = (
+            `<div class="mt-2 flex justify-between items-center p-2.5 bg-[#FAFAFA] rounded-[8px]">
+                <div>
+                    <h2 class="font-bold">
+                        ${serviceMainName}
+                    </h2>
+                    <p class="text-[#5C5C5C]">${serviceNumber}</p>
+                </div>
+                <p>
+                    ${localTime}
+                </p>
+            </div>`
+        )
+        historyContainer.appendChild(newHistory)
     })
 }
+
+
+// function for clear button
+
+document.getElementById('clear-btn').addEventListener('click', function () {
+    historyContainer.innerHTML = '';
+})
+
+
