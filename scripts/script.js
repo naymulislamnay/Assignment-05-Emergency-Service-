ea20f372d6f0a4642edd47014d89e54276e6d344
naymulislamnay <naymulislamnay@gmail.com>
2025-08-28 22:34:31 +0600

    love count and call button alert function added

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,21 @@
-document.getElementById('loved-contact-count')
+let lovedContactCount = Number(document.getElementById('loved-contact-count').innerText);
+const loveIcons = document.getElementsByClassName('love-icon');
+
+for (let loveIcon of loveIcons) {
+    loveIcon.addEventListener('click', function () {
+        lovedContactCount += 1;
+        document.getElementById('loved-contact-count').innerText = lovedContactCount;
+    })
+}
+
+// function for call button
+
+const callBtns = document.getElementsByClassName('call-btn');
+
+for (let callBtn of callBtns) {
+    callBtn.addEventListener('click', function () {
+        const serviceName = callBtn.parentNode.parentNode.childNodes[5].innerText;
+        const serviceNumber = callBtn.parentNode.parentNode.childNodes[7].innerText;
+        alert(`📞 Calling ${serviceName} ${serviceNumber}...`)
+    })
+}
